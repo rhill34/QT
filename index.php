0749d85443917e6d43d151c8356f0c445a519718
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-05-17 16:12:25 -0400

    added index, composer, and .htaccess. Also created the views and models directories.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require ("vendor/autoload.php");
+
+$f3 = Base::instance();
+
+$f3->route('GET /', function (){
+
+    //Need to create the home page.
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
