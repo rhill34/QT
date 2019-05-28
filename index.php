2cb5afb63bff2017d9042123b06c290d668fcb2f
Robert Hill <roberthill@Katherines-MacBook-Air.local>
2019-05-28 09:48:09 -0700

    Mobile View added
    Redesigned the landing page UI, connected the Register button to Mobile Responsive Traveler Information page, Renamed the Tourist information page to (Traveler Information), Added Eula age agreement consent checkbox -RH3

--- index.php
+++ index.php
@@ -5,42 +5,82 @@ error_reporting(E_ALL);
 
 session_start();
 
+//$user = $_SERVER['USER'];
+//require_once "home/$user/config.php";
+
 require_once("vendor/autoload.php");
+require_once("models/validation.php");
 
 $f3 = Base::instance();
+
+$years = array();
+//all years from 1900 to todays year
+for ($i = 1900; $i <= date("Y"); $i++)
+{
+    array_push($years,$i);
+}
+//all states
+$states = array
+(
+    'AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas', 'CA' => 'California','CO' => 'Colorado',
+    'CT' => 'Connecticut', 'DE' => 'Delaware', 'DC' => 'District Of Columbia', 'FL' => 'Florida', 'GA' => 'Georgia',
+    'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas',
+    'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland', 'MA' => 'Massachusetts',
+    'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MO' => 'Missouri', 'MT' => 'Montana',
+    'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey', 'NM' => 'New Mexico',
+    'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio', 'OK' => 'Oklahoma',
+    'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota',
+    'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia', 'WA' => 'Washington',
+    'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming'
+);
+
+
+//The following are the Routes
+
+//$f3->route('GET|POST /test', function($f3){
+//    session_destroy();
+//
+//    $view = new Template();
+////    echo $view->render('views/driverRegister.html');
+//    echo $view->render('views/driver.html');
+//});
+
 
 $f3->route('GET|POST /', function (){
     session_destroy();
     //Need to create the home page.
     $view = new Template();
-    echo $view->render('views/home2.html');
+    echo $view->render('views/landing.html');
 });
 
-$f3->route('GET|POST /basic-info', function ($f3){
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$f3->route('GET|POST /basic-info', function ($f3)
+{
 
-        //get the post information
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $phone = $_POST['phone'];
-        $email = $_POST['email'];
-
-        $f3->set('fname', $fname);
-        $f3->set('lname', $lname);
-        $f3->set('phone', $phone);
-        $f3->set('email', $email);
-
-
-        if(validatePersonal()) {
-
-
-            $f3->reroute('views/form2.html');
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $phone = stipPhone($_POST['phone']);
+        //error array
+        $arrayErr = array(
+            "fnameErr" => validName($_POST['fname']),
+            "lnameErr" => validName($_POST['lname']),
+            "phoneErr" => validPhone($phone),
+            "emailErr" => validEmail($_POST['email']),
+            "passErr" => validPass($_POST['pass'], $_POST['pass1'])
+        );
+        //check if errors array is empty
+        if (count($arrayErr) == 0) {
+            if (isset($_POST['driver'])) {
+                $_SESSION['member'] = new User_Driver(trimFilter($_POST[fname]), trimFilter($_POST[lname]), $phone);
+            } else {
+                $_SESSION['member'] = new User(trimFilter($_POST[fname]), trimFilter($_POST[lname]), $phone);
+            }
+            $f3->reroute('/interest');
         }
+        $f3->set('errors', $arrayErr);
+//        $f3->reroute('/test');
     }
-
     $view = new Template();
-    echo $view->render('views/form1.html');
+    echo $view->render('views/personal.html');
 });
 
 //Run the framework
--- models/validation.php
+++ models/validation.php
@@ -1,22 +1,149 @@
 <?php
-
-function validPersonal(){
-
-    global $f3;
-    $valid = true;
-
-    if(!validName($f3->get('fname'), $f3->get('lname'))){
-
-        $f3->set("errors['name']", "Not a valid name, please enter a valid name.");
-        $valid = false;
+/**
+ * Check string for alphabetical
+ * @param $data Represents string form data for names
+ * @return string empty string if no errors or error message
+ */
+function validName($data)
+{
+    //check if the string matches only alpha numeric
+    if (ctype_alpha($data)) {
+        return "";
+        //return array(true=>$data);
+    } else {
+        return "Invalid string. Contains numbers, special characters, or is empty.";
     }
-    if(!validEmail($f3->get('email'))){
-        $f3->set("errors['errors']", "Not a valid email address, please enter a valid email address.");
-    }
-
-    return $valid;
 }
-
-function validName($fname, $lname){
-    return (!empty($fname.$lname) && (ctype_alpha($fname.$lname)));
+/**
+ * Checks to see if matches phone number
+ * @param $data phone number inputted
+ * @return string empty string if no errors or error message
+ */
+function validPhone($data)
+{
+    //check that stripped number only 10 in length
+    if (strlen($data) == 10){
+        return "";
+    } else {
+        return "Not a valid phone number. Phone number must be 10 digits";
+    }
+}
+/**
+ * Returns a trimmed phone number only containint numbers
+ * @param $data String phone number
+ */
+function stipPhone($data)
+{
+//eliminate every char except 0-9
+    $justNums = preg_replace("/[^0-9]/", '', $data);
+    return $justNums;
+}
+/**
+ * Checks if email provided is a valid email
+ * @param $data email provided
+ * @return string empty string if no errors or error message
+ */
+function validEmail($data)
+{
+    $data = trim($data);
+    if (filter_var($data, FILTER_VALIDATE_EMAIL)) {
+        return "";
+    } else {
+        return "Invalid email. Please enter email in format You@email.com";
+    }
+}
+/**
+ * Validates that values of checkboxes selected match original check boxes values
+ * @param $data checkboxes selected values
+ * @return string empty string if no errors or error message
+ */
+function validCheckBoxArray($data , $array)
+{
+    if($data!=null) {
+        foreach ($data as $key=>$value) {
+            if (!in_array($value, $array)) {
+                return "Improper value provided. Provided value does not match checkboxes provided";
+            }
+        }
+        return "";
+    }
+    return "";
+}
+/**
+ * validates a dropdown posted value returns a value inside its array
+ * @param $data posted value for dropdown
+ * @param $dropDownArray array of values for dropdown
+ * @return bool if value is in array
+ */
+function validateDropDown($data, $dropDownArray)
+{
+    if(in_array($data, $dropDownArray, TRUE))
+    {
+        return "";
+    }
+    return "No dropdown value selected or value is invalide";
+}
+function trimFilter($data)
+{
+    return filter_var(trim($data), FILTER_SANITIZE_STRING);
+}
+/**
+ * Checks an error array has no set values if so no errors exist
+ * @param $data error array
+ * @return bool true:no errors, false:errors exist
+ */
+function checkErrArray($data)
+{
+    foreach ($data as $key => $value) {
+        if($value!="") {
+            return false;
+        }
+    }
+    return true;
+}
+/**
+ * Validate password matches regez
+ * @param $data PasswordString
+ * @return bool true:matches, false:does not match regex
+ */
+function checkPass($password)
+{
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+    if(!$uppercase)
+    {
+        return "Must contain an uppercase letter";
+    }
+    if(!$lowercase)
+    {
+        return "Must contain a lowercase letter";
+    }
+    if(!$number)
+    {
+        return "Must contain a number";
+    }
+    if(!$specialChars)
+    {
+        return "Must contain a special character";
+    }
+    if(strlen($password)<8)
+    {
+        return "Must be 8 digits long";
+    }
+    return "";
+}
+function validPass($pass, $pass2)
+{
+    if ($pass !== $pass2) {
+        return "Passwords do not match";
+    }
+    $checked=checkPass($pass);
+    if (isset($checked)) {
+        return $checked;
+    }
+    else{
+        return "";
+    }
 }
